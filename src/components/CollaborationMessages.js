b052f9edb5c0fd8ff12d5183d628f2043428cbe8
Tri Do <trido1903@gmail.com>
2020-07-18 20:59:53 -0700

    fixed scroll to view in chat message

--- src/components/CollaborationMessages.js
+++ src/components/CollaborationMessages.js
@@ -1,14 +1,23 @@
 import React from "react";
 import moment from "moment"
 
-const CollaborationMessages = ({ messages, user }) => {
-    const renderMessages = () => {
+
+class CollaborationMessages extends React.Component {
+
+    scrollTo = (ref) => {
+        console.log(ref);
+        if (ref) {
+            ref.scrollIntoView();
+        }
+    }
+
+    renderMessages = ({ messages, user }) => {
         if (messages.length > 0) {
             return messages.map(message => {
                 // Message is from currently logged in USER
                 if (message.user.uid === user.id) {
                     return (
-                        <div key={message.id} className="viewWrapItemRight">
+                        <div ref={this.scrollTo} key={message.id} className="viewWrapItemRight">
                             <div className="viewWrapItemRight3">
                                 <img
                                     src={message.user.avatar}
@@ -24,7 +33,7 @@ const CollaborationMessages = ({ messages, user }) => {
                 }
 
                 return (
-                    <div key={message.id} className="viewWrapItemLeft">
+                    <div ref={this.scrollTo} key={message.id} className="viewWrapItemLeft">
                         <div className="viewWrapItemLeft3">
                             <img
                                 src={message.user.avatar}
@@ -39,10 +48,56 @@ const CollaborationMessages = ({ messages, user }) => {
                 )
             })
         }
-
         return null
     }
-    return renderMessages()
+
+    render() {
+        return this.renderMessages(this.props);
+    }
 }
+
+// const CollaborationMessages = ({ messages, user }) => {
+//     const renderMessages = () => {
+//         if (messages.length > 0) {
+//             return messages.map(message => {
+//                 // Message is from currently logged in USER
+//                 if (message.user.uid === user.id) {
+//                     return (
+//                         <div key={message.id} className="viewWrapItemRight">
+//                             <div className="viewWrapItemRight3">
+//                                 <img
+//                                     src={message.user.avatar}
+//                                     alt="avatar"
+//                                     className="peerAvatarLeft" />
+//                                 <div className="viewItemRight">
+//                                     <span className="textContentItem">{message.content}</span>
+//                                 </div>
+//                             </div>
+//                             <span className="textTimeLeft">{moment(message.timestamp).fromNow()}</span>
+//                         </div>
+//                     )
+//                 }
+
+//                 return (
+//                     <div key={message.id} className="viewWrapItemLeft">
+//                         <div className="viewWrapItemLeft3">
+//                             <img
+//                                 src={message.user.avatar}
+//                                 alt="avatar"
+//                                 className="peerAvatarLeft" />
+//                             <div className="viewItemLeft">
+//                                 <span className="textContentItem">{message.content}</span>
+//                             </div>
+//                         </div>
+//                         <span className="textTimeLeft">{moment(message.timestamp).fromNow()}</span>
+//                     </div>
+//                 )
+//             })
+//         }
+
+//         return null
+//     }
+//     return renderMessages()
+// }
 
 export default CollaborationMessages;
--- src/reducers/collaborationReducer.js
+++ src/reducers/collaborationReducer.js
@@ -13,8 +13,6 @@ const INITIAL_STATE = {
     messages: []
 }
 
-
-
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case FETCH_COLLABORATION:
