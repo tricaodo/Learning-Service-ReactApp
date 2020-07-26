b595d53c16e6ed96acb1b410fab321fad5b980c5
Tri Do <trido1903@gmail.com>
2020-07-25 19:53:58 -0700

    deploying learning service

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import reducer from "./reducers";
 import thunk from "redux-thunk";
 import { ToastProvider } from "react-toast-notifications";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const store = createStore(reducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, /* preloadedState, */ applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
     <ToastProvider>
